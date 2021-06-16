49bf83c155db0e4f878991a2e1dfee6455486b76
catrovac <84932550+catrovac@users.noreply.github.com>
2021-06-16 14:45:41 +0300

    Update index_bot.py

--- index_bot.py
+++ index_bot.py
@@ -1,4 +1,3 @@
-import selenium
 from telethon.tl.functions.messages import GetBotCallbackAnswerRequest
 import telethon
 import time
@@ -6,11 +5,13 @@ import requests
 import bs4
 import os
 from selenium import webdriver
-import pprint
+import selenium
+#import pprint
 # import popen
 from setting import api, api_hash
 from sql import content
 from sql import vallet
+from sql import headers
 
 while True:
     name = content[0][0]
@@ -45,10 +46,9 @@ while True:
                     rec = messages[0].reply_markup.rows[1].buttons[1].data
                     rec_id = messages[0].id
                     if rec_url:
-                        print(bot_name)
                         if text_button:
                             s = requests.session()
-                            result = s.get(rec_url, allow_redirects=True).text
+                            result = s.get(rec_url, allow_redirects=True,headers=headers).text
                             time.sleep(2)
                             soup = bs4.BeautifulSoup(result, 'lxml')
                             data = soup.find('title')
@@ -60,6 +60,7 @@ while True:
                             s.cookies.keys()
                             s.cookies.clear()
                             if 'just' in datax.lower():
+                                print(bot_name)
                                 print(f'на страничке {rec_url} есть капча, открываю браузер по умолчанию')
                                 os.system(f"start \"\" {rec_url}")
                                 time.sleep(6)
@@ -73,6 +74,7 @@ while True:
                                 os.system("TASKKILL /F /IM chrome.exe")
                                 os.system(("TASKKILL /F /IM software_reporter_tool.exe"))
                             else:
+                                print(bot_name)
                                 firefox = webdriver.Firefox()
                                 firefox.get(rec_url)
                                 time.sleep(5)
@@ -92,14 +94,12 @@ while True:
                 time.sleep(1)
                 ball = await client.get_messages(bot_name, limit=1)
                 ballx = ball[0].message
-                qx = bot_name[0:3]
+                qx = bot_name.split(" ")[0]
+                print(qx)
                 q = str(ballx)
-                x = q.replace('Available balance:', '')
-                qz = x.replace(qx, '')
-                xz = qz.split()
-                zx1 = xz[0]
-                print(zx1, bot_name)
-                if '0.00022' in zx1 and bot_name != 'DOGE Click Bot' and bot_name != 'BTC Click Bot':
+                x = q.replace('Available balance:', '').split()[0]
+                print(x, bot_name)
+                if float(x) > float(0.00022) and bot_name != 'DOGE Click Bot' and bot_name != 'BTC Click Bot':
                     await client.send_message(bot_name, 'Withdraw')
                     time.sleep(1)
                     vals = vallet[qx]
@@ -108,22 +108,23 @@ while True:
                     await client.send_message(bot_name, 'Max amount')
                     time.sleep(1)
                     await client.send_message(bot_name, 'confirm')
-                elif bot_name == 'DOGE Click Bot' and '2.1' in zx1:
+                elif bot_name == 'DOGE Click Bot' and float(2.1) < float(x):
                     await client.send_message(bot_name, 'Withdraw')
                     time.sleep(1)
                     zxbot = list(bot_name.split())
-                    await client.send_message(bot_name, vallet[zxbot[0]])
+                    await client.send_message(bot_name, vallet[qx])
                     time.sleep(1)
                     await client.send_message(bot_name, 'Max amount')
                     time.sleep(1)
                     await client.send_message(bot_name, 'confirm')
-                elif bot_name == 'BTC Click Bot' and '0.00005' in zx1:
-                    # await client.send_message(bot_name, 'Withdraw')
+                elif bot_name == 'BTC Click Bot' and float(0.000050) < float(x):
+                    # await client.send_message(bot_name, 'withraw')
                     time.sleep(1)
-                    x_bot = list(bot_name.split())
+                    x_bot = list(bot_name.split())[0]
+                    print(x_bot)
                     await client.send_message(bot_name, 'Withdraw')
                     time.sleep(1)
-                    await client.send_message(bot_name, vallet[x_bot[0]])
+                    await client.send_message(bot_name, vallet[qx])
                     time.sleep(1)
                     await client.send_message(bot_name, 'Max amount')
                     time.sleep(1)
@@ -138,7 +139,13 @@ while True:
                 resp = await client(GetBotCallbackAnswerRequest(bot_name, rec_id, data=rec))
                 resp
                 print(redirect)
-        time.sleep(90)
+            except selenium.common.exceptions.TimeoutException as timeexcept:
+                print(timeexcept)
+            except NameError as name:
+                print(name)
+            except ValueError as val:
+                print(val)
+        time.sleep(20)
 
 
     with client:
